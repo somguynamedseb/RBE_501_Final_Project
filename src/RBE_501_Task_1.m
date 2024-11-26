2e9b0c9b2501f3fb5b6ae040238cd3f4734dee19
Sebastian Baldini <sebastian.baldini@outlook.com>
2024-11-25 19:34:38 -0500

    it no workie:(

--- src/RBE_501_Task_1.m
+++ src/RBE_501_Task_1.m
@@ -1,4 +1,8 @@
 close all;
+clc;
+clear;
+
+% Set up robot and gclose all;
 clear;
 clc;
 
@@ -22,6 +26,41 @@ pt_B = [[0,0,1,0.185];
 % pause(1);
 % robot.measured_js(1,0)
 % robot.interpolate_jp(cal_home, 2000)
+% pause(4);
+% robot.measured_js(1,0)
+% robot.interpolate_jp(rad2deg(theta_A), 2000)
+% pause(4);
+% robot.measured_js(1,0)
+% robot.interpolate_jp(rad2deg(theta_B), 2000)
+% pause(1);
+
+
+o home
+robot = Robot();
+cal_home = [0,0,0,0];
+
+
+%% Task 1
+
+PhiA = -pi/4;
+PhiB = atan(0.17,0.185);
+
+pt_A = [[0,-sin(PhiA),-cos(PhiA),0.185];
+    [0,cos(PhiA),-sin(PhiA),-0.185];
+    [1,0,0,0.185];
+    [0,0,0,1]];
+
+
+pt_B = [[0,-sin(PhiB),-cos(PhiB),0.185];
+    [0,cos(PhiN),-sin(PhiB),0.17];
+    [1,0,0,0.07];
+    [0,0,0,1]];
+
+[theta_A,s] = robot.ikspace(pt_A)
+[theta_B,s] = robot.ikspace(pt_B)
+pause(1);
+robot.measured_js(1,0)
+robot.interpolate_jp(cal_home, 2000)
 % pause(4);
 % robot.measured_js(1,0)
 % robot.interpolate_jp(rad2deg(theta_A), 2000)
