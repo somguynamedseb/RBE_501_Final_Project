0fe319238b539c4cf293ead537fd4521152df4e2
rhit-leeba <leeba@rose-hulman.edu>
2024-12-01 12:28:26 -0500

    push

--- src/RBE_501_Task_1.m
+++ src/RBE_501_Task_1.m
@@ -1,3 +1,16 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% RBE_501_Task_1.m
+%
+% Input: None, positions are hardcoded
+%
+% Output: Robot moves to home, then A, then B
+%
+% Written by Sebastian Baldani
+%
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
 close all;
 clc;
 clear;
@@ -12,61 +25,30 @@ cal_home = [0,0,0,0];
 
 
 %% Task 1
-pt_A = [[0,0,1,0.185];
-    [0,1,0,-0.185];
+% Define transformations from A and B
+phiA = -pi/4;
+phiB = atan2(0.17,0.185);
+T_A = [[0,-sin(phiA),-cos(phiA),0.185];
+    [0,cos(phiA),-sin(phiA),-0.185];
     [1,0,0,0.185];
     [0,0,0,1]]
-pt_B = [[0,0,1,0.185];
-    [0,1,0,0.170];
+T_B = [[0,-sin(phiB),-cos(phiB),0.185];
+    [0,cos(phiB),-sin(phiB),0.170];
     [1,0,0,0.07];
     [0,0,0,1]]
 
-[theta_A,s] = robot.ikspace(pt_A)
-[theta_B,s] = robot.ikspace(pt_B)
-% pause(1);
-% robot.measured_js(1,0)
-% robot.interpolate_jp(cal_home, 2000)
-% pause(4);
-% robot.measured_js(1,0)
-% robot.interpolate_jp(rad2deg(theta_A), 2000)
-% pause(4);
-% robot.measured_js(1,0)
-% robot.interpolate_jp(rad2deg(theta_B), 2000)
-% pause(1);
+% Get joint angles for A and B
+[theta_A,s] = robot.ikspace(T_A)
+[theta_B,s] = robot.ikspace(T_B)
 
-
-o home
-robot = Robot();
-cal_home = [0,0,0,0];
-
-
-%% Task 1
-
-PhiA = -pi/4;
-PhiB = atan(0.17,0.185);
-
-pt_A = [[0,-sin(PhiA),-cos(PhiA),0.185];
-    [0,cos(PhiA),-sin(PhiA),-0.185];
-    [1,0,0,0.185];
-    [0,0,0,1]];
-
-
-pt_B = [[0,-sin(PhiB),-cos(PhiB),0.185];
-    [0,cos(PhiN),-sin(PhiB),0.17];
-    [1,0,0,0.07];
-    [0,0,0,1]];
-
-[theta_A,s] = robot.ikspace(pt_A)
-[theta_B,s] = robot.ikspace(pt_B)
+% Move to home, then A, then B
 pause(1);
-robot.measured_js(1,0)
-robot.interpolate_jp(cal_home, 2000)
-% pause(4);
-% robot.measured_js(1,0)
-% robot.interpolate_jp(rad2deg(theta_A), 2000)
-% pause(4);
-% robot.measured_js(1,0)
-% robot.interpolate_jp(rad2deg(theta_B), 2000)
-% pause(1);
-
-
+robot.measured_js(1,0,0)
+robot.interpolate_jp(cal_home, 2000) % position, time in milliseconds
+pause(4);
+robot.measured_js(1,0,0)
+robot.interpolate_jp(rad2deg(theta_A), 2000)
+pause(4);
+robot.measured_js(1,0,0)
+robot.interpolate_jp(rad2deg(theta_B), 500)
+pause(1);
